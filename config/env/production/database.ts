6edd5817657cec08975fdc4c154f03090a4a10d6
Francisco Fetapi <franciscofetapi10@gmail.com>
2022-12-01 10:48:07 +0100

    feat: configuring mysql as database for production

---
+++ config/env/production/database.ts
@@ -0,0 +1,20 @@
+import path from "path";
+
+export default ({ env }) => ({
+  connection: {
+    // client: 'sqlite',
+    // connection: {
+    //   filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    // },
+    // useNullAsDefault: true,
+    client: "mysql",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "default"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", ""),
+    },
+    useNullAsDefault: true,
+  },
+});
