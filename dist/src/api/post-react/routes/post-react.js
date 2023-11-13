b9f1d041472a3b1f735ea5032019c2fc2d30089f
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-11-13 09:31:10 +0100

    refactor: dist deleted

--- dist/src/api/post-react/routes/post-react.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * post-react router
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::post-react.post-react');
